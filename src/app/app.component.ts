b8dcf35e016b42b23f005844e7069bf855e600d1
Stephen Fluin <stephen.fluin@gmail.com>
2017-05-30 17:38:16 -0700

    Initial spinner design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  rotation = 0;
+
+  startTouchPosX;
+  startTouchPosY;
+
+  touchstart(event) {
+    console.log('TouchStart',event);
+    this.startTouchPosX = event.changedTouches[0].clientX;
+    this.startTouchPosY = event.changedTouches[0].clientY;
+
+  }
+  touchend(event) {
+    console.log('TouchEnd',event);
+    this.rotation = this.startTouchPosX - event.changedTouches[0].clientX;
+  }
+  touchmove(event) {
+    console.log('TouchMove',event);
+    this.rotation = this.startTouchPosX - event.changedTouches[0].clientX;
+  }
 }
