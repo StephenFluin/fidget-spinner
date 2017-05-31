f54de6a75c93f6875f64cb00f0af7558d86353a6
Stephen Fluin <stephen.fluin@gmail.com>
2017-05-31 09:03:09 -0700

    Add spinner momentum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,61 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  /**
+   * The physical rotation of the spinner at a given time.
+   */
   rotation = 0;
+  /**
+   * How far from starting point we are. Used only when handling
+   */
+  rotationOffset = 0;
+  rotationMomentum = 0;
 
   startTouchPosX;
   startTouchPosY;
+  startAngle;
 
-  touchstart(event) {
-    console.log('TouchStart',event);
+  momentumTimeout;
+
+  constructor() {
+    this.handleMomentum();
+  }
+
+  start(event) {
     this.startTouchPosX = event.changedTouches[0].clientX;
     this.startTouchPosY = event.changedTouches[0].clientY;
+    this.startAngle = this.getAngleOfTouch(this.startTouchPosX, this.startTouchPosY)
+    this.rotationMomentum = 0;
+  }
+  end(event) {
+    this.rotationOffset = this.rotation;
+    let angle = this.getAngleOfTouch(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    this.rotationMomentum = angle - this.startAngle;
+    this.handleMomentum();
+  }
+  move(event) {
+    let angle = this.getAngleOfTouch(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+    this.setRotation(angle - this.startAngle);
+  }
+  setRotation(angle) {
+    this.rotation = angle + this.rotationOffset;
+  }
 
+
+  getAngleOfTouch(touchX, touchY) {
+    const { height: height, width: width, top: top, left: left } = document.getElementById('spinner').getBoundingClientRect();
+    const [centerX, centerY] = [width / 2 + left, height / 2 + top];
+    const angle = Math.atan2(touchY - centerY, touchX - centerX) * 180 / Math.PI;
+    return angle;
   }
-  touchend(event) {
-    console.log('TouchEnd',event);
-    this.rotation = this.startTouchPosX - event.changedTouches[0].clientX;
-  }
-  touchmove(event) {
-    console.log('TouchMove',event);
-    this.rotation = this.startTouchPosX - event.changedTouches[0].clientX;
+
+  handleMomentum = () => {
+    this.rotation += this.rotationMomentum;
+    this.rotationMomentum = this.rotationMomentum * 0.95;
+    if(this.rotationMomentum < 10) {
+      this.rotationMomentum = 0;
+    } else {
+      requestAnimationFrame(this.handleMomentum);
+    }
   }
 }
